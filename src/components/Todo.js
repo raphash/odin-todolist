98f08f289ed9ed2ba1a7f5f1f3df4a0583db0026
raphash <175216103+raphash@users.noreply.github.com>
2025-01-17 06:53:47 -0300

    style: Add eye icon to todo card to implement inspect feature

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -25,6 +25,7 @@ export function createCard(todo) {
                             <time datetime="${todo.dueDate}">${todo.dueDate}</time>
                           </p>
                           <div class="buttons">
+                            <box-icon name='show' color="#a1a1aa" class="show-todo"></box-icon>
                             <box-icon name='edit' color="#5c73f6" class="edit-todo"></box-icon>
                             <box-icon name='trash-alt' color="#f65c5c" class="delete-todo"></box-icon>
                           </div>
